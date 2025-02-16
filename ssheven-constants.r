424388bcc70cbf76fd15c0e3d92c8ec785ae3d87
cy384 <cy384@cy384.com>
2025-02-16 17:09:01 -0500

    bump version

--- ssheven-constants.r
+++ ssheven-constants.r
@@ -4,11 +4,11 @@
 #define __SSHEVEN_CONSTANTS_R__
 
 /* so many versions */
-#define SSHEVEN_VERSION             "0.8.9"
-#define SSHEVEN_LONG_VERSION        "0.8.9 prerelease, by cy384"
-#define SSHEVEN_DESCRIPTION         "ssheven 0.8.9 by cy384"
+#define SSHEVEN_VERSION             "0.9.0"
+#define SSHEVEN_LONG_VERSION        "0.9.0 prerelease, by cy384"
+#define SSHEVEN_DESCRIPTION         "ssheven 0.9.0 by cy384"
 #define SSHEVEN_VERSION_MAJOR       0x00
-#define SSHEVEN_VERSION_MINOR       0x89
+#define SSHEVEN_VERSION_MINOR       0x90
 #define SSHEVEN_VERSION_PRERELEASE  0x01
 
 /* options: development, alpha, beta, release */
